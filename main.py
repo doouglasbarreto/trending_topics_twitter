8af993ae6f87bc9bd8171ce2ac1d8919242935f5
Douglas Barreto <doouglas_barreto@hotmail.com>
2022-02-11 22:10:56 -0300

    Update

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
+import os
+import sys
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QLabel
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-
 
 print("Iniciando nosso robô... \n")
-
-driver = webdriver.Chrome("C:/Users/JMTech/Desktop/Robos/chromedriver")
+dir_path = os.getcwd()
+chromedriver = os.path.join(dir_path, "chromedriver.exe")
+driver = webdriver.Chrome(chromedriver)
 
 driver.get('https://trends24.in/brazil/')
 pesquisa = driver.find_element(By.CLASS_NAME, 'page-content__blurb').text
-x = pesquisa.replace("Today's Top Twitter Trending Brazil topics are ", "")
-print(x)
+aux = "".join(pesquisa.splitlines())
+x = aux.replace("Today's Top Twitter Trending Brazil topics are ", "")
 
-y = pesquisa.find("Nazismo")
-print(y)
+y = x.find(". Tweet")
 final = x[:y]
 
-print(final)
+final2 = final.split(",")
+final2[0] = " " + final2[0]
+
+contador = 0
+meufinal = ""
+for a in final2:
+    contador += 1
+    meufinal += str(contador) + "º -" + str(a) + "\n"
+
+print(meufinal)
+
+
+def basicWindow():
+    app = QtWidgets.QApplication(sys.argv)
+    windowExample = QtWidgets.QWidget()
+    windowExample.setFixedWidth(300)
+    windowExample.setFixedHeight(300)
+    windowExample.setWindowTitle('Trending Topics')
+    windowExample.label = QLabel(meufinal, windowExample)
+    windowExample.show()
+    sys.exit(app.exec_())
+
 driver.close()
+basicWindow()
+
